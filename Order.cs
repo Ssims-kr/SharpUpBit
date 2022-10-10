9ec5a170e59edb2adc42be0f223d1c21190fc217
씸즈 <36888398+Ssims-kr@users.noreply.github.com>
2022-10-10 17:15:43 +0900

    Update Order.cs

--- Order.cs
+++ Order.cs
@@ -1,9 +1,8 @@
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Serialization;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace SharpUpBit
@@ -148,11 +147,170 @@ namespace SharpUpBit
                 public Account AskAccount { get; set; }
                 #endregion
             }
+
+            /// <summary>
+            /// 주문건 정보
+            /// </summary>
+            public class OrderInfo {
+                #region 클래스
+                /// <summary>
+                /// 체결 정보
+                /// </summary>
+                public class TradeInfo {
+                    #region 프로퍼티
+                    /// <summary>
+                    /// 마켓의 유일키
+                    /// </summary>
+                    [JsonProperty(PropertyName = "market")]
+                    public string Market { get; set; }
+
+                    /// <summary>
+                    /// 체결의 고유 아이디
+                    /// </summary>
+                    [JsonProperty(PropertyName = "uuid")]
+                    public string UUID { get; set; }
+
+                    /// <summary>
+                    /// 체결의 가격
+                    /// </summary>
+                    [JsonProperty(PropertyName = "price")]
+                    public double Price { get; set; }
+
+                    /// <summary>
+                    /// 체결 양
+                    /// </summary>
+                    [JsonProperty(PropertyName = "volume")]
+                    public double Volume { get; set; }
+
+                    /// <summary>
+                    /// 체결된 총 가격
+                    /// </summary>
+                    [JsonProperty(PropertyName = "funds")]
+                    public double Funds { get; set; }
+
+                    /// <summary>
+                    /// 체결 종류
+                    /// </summary>
+                    [JsonProperty(PropertyName = "side")]
+                    public string Side { get; set; }
+
+                    /// <summary>
+                    /// 체결 시각
+                    /// </summary>
+                    [JsonProperty(PropertyName = "created_at")]
+                    public DateTime CreatedAt { get; set; }
+                    #endregion
+                }
+                #endregion
+
+                #region 프로퍼티
+                /// <summary>
+                /// 주문의 고유 아이디
+                /// </summary>
+                [JsonProperty(PropertyName="uuid")]
+                public string UUID { get; set; }
+
+                /// <summary>
+                /// 주문 종류
+                /// </summary>
+                [JsonProperty(PropertyName="side")]
+                public string Side { get; set; }
+
+                /// <summary>
+                /// 주문 방식
+                /// </summary>
+                [JsonProperty(PropertyName="ord_type")]
+                public string OrderType { get; set; }
+
+                /// <summary>
+                /// 주문 당시 화폐 가격
+                /// </summary>
+                [JsonProperty(PropertyName="price")]
+                public double Price { get; set; }
+
+                /// <summary>
+                /// 주문 상태
+                /// </summary>
+                [JsonProperty(PropertyName="state")]
+                public string State { get; set; }
+
+                /// <summary>
+                /// 마켓의 유일키
+                /// </summary>
+                [JsonProperty(PropertyName="market")]
+                public string Market { get; set; }
+
+                /// <summary>
+                /// 주문 생성 시간
+                /// </summary>
+                [JsonProperty(PropertyName="created_at")]
+                public DateTime CreatedAt { get; set; }
+
+                /// <summary>
+                /// 사용자가 입력한 주문 양
+                /// </summary>
+                [JsonProperty(PropertyName="volume")]
+                public double Volume { get; set; }
+
+                /// <summary>
+                /// 체결 후 남은 주문 양
+                /// </summary>
+                [JsonProperty(PropertyName="remaining_volume")]
+                public double RemainingVolume { get; set; }
+
+                /// <summary>
+                /// 수수료로 예약된 비용
+                /// </summary>
+                [JsonProperty(PropertyName="reserved_fee")]
+                public double ReservedFee { get; set; }
+
+                /// <summary>
+                /// 남은 수수료
+                /// </summary>
+                [JsonProperty(PropertyName="remaining_fee")]
+                public double RemainingFee { get; set; }
+
+                /// <summary>
+                /// 사용된 수수료
+                /// </summary>
+                [JsonProperty(PropertyName="paid_fee")]
+                public double PaidFee { get; set; }
+
+                /// <summary>
+                /// 거래에 사용중인 비용
+                /// </summary>
+                [JsonProperty(PropertyName="locked")]
+                public double Locked { get; set; }
+
+                /// <summary>
+                /// 체결된 양
+                /// </summary>
+                [JsonProperty(PropertyName="executed_volume")]
+                public double ExecutedVolume { get; set; }
+
+                /// <summary>
+                /// 해당 주문에 걸린 체결 수
+                /// </summary>
+                [JsonProperty(PropertyName="trades_count")]
+                public int TradesCount { get; set; }
+
+                /// <summary>
+                /// 체결 정보
+                /// </summary>
+                [JsonProperty(PropertyName="trades")]
+                public TradeInfo[] Trade { get; set; }
+                #endregion
+            }
             #endregion
         }
         #endregion
 
         #region 메서드
+        /// <summary>
+        /// 마켓별 주문 가능 정보를 취득합니다.
+        /// </summary>
+        /// <param name="marketId">마켓 ID</param>
+        /// <returns>마켓별 주문 가능 정보</returns>
         public async Task<Order.OrderChance> GetOrderChance(string marketId) {
             var client = new System.Net.Http.HttpClient();
             var request = new System.Net.Http.HttpRequestMessage
@@ -172,7 +330,6 @@ namespace SharpUpBit
                 var body = await response.Content.ReadAsStringAsync();
 
                 JObject jObj = JObject.Parse(body);
-                Console.WriteLine(jObj);
 
                 rtn.BidFee = double.Parse(jObj["bid_fee"].ToString());
                 rtn.AskFee = double.Parse(jObj["ask_fee"].ToString());
@@ -209,6 +366,53 @@ namespace SharpUpBit
                 //rtn.AskAccount.UnitCurrency = jObj["ask_account"]["unit_currency"].ToString();
                 rtn.BidAccount = JsonConvert.DeserializeObject<Account>(jObj["bid_account"].ToString());
                 rtn.AskAccount = JsonConvert.DeserializeObject<Account>(jObj["ask_account"].ToString());
+            }
+
+            return rtn;
+        }
+
+        /// <summary>
+        /// 주문 UUID를 통해 개별 주문건을 취득합니다.
+        /// </summary>
+        /// <param name="uuid">주문 UUID</param>
+        /// <returns>개별 주문건 정보</returns>
+        public async Task<Order.OrderInfo> GetOrderInfo(string uuid) {
+            var client = new System.Net.Http.HttpClient();
+            var request = new System.Net.Http.HttpRequestMessage
+            {
+                Method = System.Net.Http.HttpMethod.Get,
+                RequestUri = new Uri(@"https://api.upbit.com/v1/order?uuid=" + uuid),
+                Headers =
+                {
+                    { "accept", "application/json" },
+                    { "Authorization", this.GetJwtToken(this.GetQueryString(new Dictionary<string, string>() { { "uuid", uuid} })) },
+                },
+            };
+
+            Order.OrderInfo rtn = new Order.OrderInfo();
+
+            using (var response = await client.SendAsync(request)) {
+                var body = await response.Content.ReadAsStringAsync();
+
+                JObject jObj = JObject.Parse(body);
+                Console.WriteLine(jObj.ToString());
+
+                rtn.UUID = jObj["uuid"].ToString();
+                rtn.Side = jObj["side"].ToString();
+                rtn.OrderType = jObj["ord_type"].ToString();
+                rtn.Price = double.Parse(jObj["price"].ToString());
+                rtn.State = jObj["state"].ToString();
+                rtn.Market = jObj["market"].ToString();
+                rtn.CreatedAt = DateTime.Parse(jObj["created_at"].ToString());
+                rtn.Volume = double.Parse(jObj["volume"].ToString());
+                rtn.RemainingVolume = double.Parse(jObj["remaining_volume"].ToString());
+                rtn.ReservedFee = double.Parse(jObj["reserved_fee"].ToString());
+                rtn.RemainingFee = double.Parse(jObj["remaining_fee"].ToString());
+                rtn.PaidFee = double.Parse(jObj["paid_fee"].ToString());
+                rtn.Locked = double.Parse(jObj["locked"].ToString());
+                rtn.ExecutedVolume = double.Parse(jObj["executed_volume"].ToString());
+                rtn.TradesCount = int.Parse(jObj["trades_count"].ToString());
+                rtn.Trade = jObj.SelectToken("trades").ToObject<Order.OrderInfo.TradeInfo[]>();
             }
 
             return rtn;
